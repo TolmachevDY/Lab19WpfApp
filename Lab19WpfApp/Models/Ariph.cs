e684f8bb8ff158a38e1b305d4dc5f6d10b0ce15a
Dmitry Tolmachev <tolmachevdy@mail.ru>
2023-01-06 23:47:02 +0300

    Добавьте файлы проекта.

---
+++ Lab19WpfApp/Models/Ariph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab19WpfApp.Models
+{
+    static class Ariph
+    {
+        public static double Add(double a) => (double)(2.0 * Math.PI * a);
+    }
+}
